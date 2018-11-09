04a15687cb911dd2cd450dae05b34f95d923a69d
Chrismarcel <cmarcelj@gmail.com>
2018-11-09 10:53:44 +0100

    feat: Setup router and route handlers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 
+// Server setup
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
@@ -13,17 +14,52 @@ const server = http.createServer((req, res) => {
 
   const decoder = new StringDecoder("utf-8");
 
-  let buffer = "";
+  let payload = "";
   req.on("data", data => {
-    buffer += decoder.write(data);
+    payload += decoder.write(data);
   });
 
   req.on("end", _ => {
-    buffer += decoder.end();
+    payload += decoder.end();
+    const chosenHandler = !router[trimmedPath]
+      ? handlers.notFound
+      : router[trimmedPath];
 
-    console.log(headers, method, buffer, queryStringObject, trimmedPath);
-    res.end(typeof queryStringObject);
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload
+    };
+
+    chosenHandler(data, function(statusCode = 200, payload = {}) {
+      statusCode = !Number(statusCode) ? 200 : statusCode;
+      payload = typeof payload != "object" ? {} : payload;
+      const payloadString = JSON.stringify(payload);
+
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log("We dumping these responses", statusCode, payloadString);
+    });
   });
 });
+
+// Route handlers
+const handlers = {};
+
+handlers.sample = function(data, callback) {
+  callback(406, { name: "sample handler" });
+};
+
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+// Router setup
+const router = {
+  sample: handlers.sample
+};
 
 server.listen(3000, _ => console.log("Server is listening on port 3000"));
